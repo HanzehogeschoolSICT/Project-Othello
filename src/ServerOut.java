e46a4c4bf6e93523bd2dd3d519187b133d36b8e4
Georg Groenendaal <georg.groenendaal@gmail.com>
2017-03-26 19:55:15 +0200

    Execute commands from the console

--- src/ServerOut.java
+++ src/ServerOut.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by georg on 24-Mar-17.
  */
 public class ServerOut {
+
+    private ServerIn serverIn;
+    private PrintWriter out;
 
     /**
      * Connects with the game server
@@ -17,21 +21,19 @@ public class ServerOut {
     ServerOut(String name, String hostname, int port) throws IOException {
 
         Socket socket = new Socket(hostname, port);
-        ServerIn serverIn;
 
         if (socket.isConnected()) {
-            System.out.println("connected");
+            System.out.println("Connected on: " + hostname + ":" + port);
         }
 
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out = new PrintWriter(socket.getOutputStream(), true);
 
         serverIn = new ServerIn(socket.getInputStream());
         new Thread(serverIn).start();
 
-
         // Small connection delay
         try {
-            Thread.sleep(200);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -39,10 +41,31 @@ public class ServerOut {
         out.println("login " + name);
     }
 
+    /**
+     * Execute a command fromt System.in
+     */
+    public void readFromConsole(){
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
+
+        // Stop listening thread
+        if (input.equals("quit")) {
+            System.out.println("Disconnecting server");
+            serverIn.disconnect();
+            out.println("quit");
+            return;
+        }
+
+        out.println(input);
+        readFromConsole();
+    }
+
     public static void main( String[] args ) {
         try {
-            new ServerOut("Georg", "localhost", 7789);
-        } catch (IOException e) { 
+            ServerOut server1 = new ServerOut("Groep 7", "localhost", 7789);
+            server1.readFromConsole();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
