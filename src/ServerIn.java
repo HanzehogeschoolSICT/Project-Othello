7d384be9c5486d4f569cad930d1cd690f1ad6760
Rogier de Boer <Rogier de Boer>
2017-04-04 12:29:21 +0200

    Added orthello random AI

--- src/ServerIn.java
+++ src/ServerIn.java
@@ -1,37 +1,132 @@
 import sun.net.ConnectionResetException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /**
  * Created by georg on 24-Mar-17.
  */
 public class ServerIn implements Runnable {
 
-    InputStreamReader inReader;
+    BufferedReader inReader;
     boolean stop = false;
+    private boolean Connected = false;
+    private String message = "";
+    private String move = "";
 
     ServerIn(InputStream in) throws IOException {
-        inReader = new InputStreamReader(in);
+        inReader = new BufferedReader(new InputStreamReader(in));
     }
 
+    private void parse(String line) throws IOException {
+        if (line.equals("OK")) {
+            System.out.println("Commando ontvangen/succesvol");
+        }
+
+        // SVR commando met value
+        if (line.matches("^SVR.*")) {
+            System.out.println("Value ontvangen");
+            SVRparser(line);
+        }
+        if (line.contains("SVR GAME MATCH")) {
+            Connected = true;
+        }
+        if (line.contains("PLAYERTOMOVE")) {
+            message = line;
+        }
+        if (line.contains("SVR GAME MOVE")) {
+            System.out.print("");
+            move = line;
+        }
+    }
+
+    /**
+     * Parses an response with a value in it
+     *
+     * @param line
+     */
+    private void SVRparser(String line) throws IOException {
+        // Gamelist return
+        if (line.matches("^SVR GAMELIST.*")) {
+            System.out.println(parseArray(line));
+            System.out.println("playerlist done");
+        }
+
+        // Playerlist return
+        if (line.matches("^SVR PLAYERLIST.*")) {
+            parseArray(line);
+        }
+        if (line.contains("SVR GAME MATCH")) {
+            Connected = true;
+        }
+    }
+
+    /**
+     * Parses the array vale from the Input string
+     * Example: SVR GAMELIST {"HELLO", "WORLD"}
+     * Returns arraylist with HELLO and WORLD
+     * TODO prevent wrong parsing with strings containing " character. Example: SVR gamelist {"not", "work" ing"}
+     *
+     * @param line
+     * @return
+     */
+    private ArrayList<String> parseArray(String line) {
+        Pattern p = Pattern.compile("\"(.*?)\"");
+        Matcher matcher = p.matcher(line);
+        ArrayList<String> acc = new ArrayList<>();
+
+        System.out.println(matcher.groupCount());
+//        for (int i = 0; i < matcher.groupCount(); i++){
+//            System.out.println("loop");
+//            System.out.println(matcher.group(i));
+//            acc.add(matcher.group(i));
+//        }
+
+        return acc;
+    }
+
+    /**
+     * Parse input string with key value pairs.
+     */
+//    private Map<String, String> parseMap(String line){
+//
+//    }
     @Override
     public void run() {
-        while(!stop){
+        while (!stop) {
             try {
-                System.out.print((char) inReader.read());
+                String inLine = inReader.readLine();
+                System.out.println(inLine);
+                this.parse(inLine);
             } catch (ConnectionResetException e) {
                 stop = true;
                 System.out.println("Closing listening connection");
             } catch (IOException e) {
+                stop = true;
                 e.printStackTrace();
             }
         }
     }
 
-    public void disconnect(){
+    public void disconnect() {
         stop = true;
+    }
+
+    public boolean getConnected() {
+        return Connected;
+    }
+
+    public String getMsg() {
+        return message;
+    }
+    public String getMove() {
+        System.out.print("");
+        return move;
     }
 }
--- src/TicTacToeAI.java
+++ src/TicTacToeAI.java
@@ -2,54 +2,53 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class TicTacToeAI extends AIClass {
-	ArrayList<Integer> movesDone = new ArrayList<Integer>();
-	int move;
-	
-	public TicTacToeAI(){
-		super.board = "#########";
-	}
-	
-	@Override
-	public int getNewMove(int input) {
-		if(movesDone.contains(input)){
-			return -1;
-		}
-		//processMove(input,'p');				the control class is supposed to process the moves depending on if the server accepts them
+    ArrayList<Integer> movesDone = new ArrayList<Integer>();
+    int move;
+
+    public TicTacToeAI(){
+        super.board = "#########";
+    }
+
+    @Override
+    public int getNewMove(int input) {
+        if(movesDone.contains(input) || movesDone.size() == 9){
+            return -1;
+        }
+        processMove(input, 'p');				//the control class is supposed to process the moves depending on if the server accepts them
+        processMove(calculateMove(), 'c');
+        return move;
+    }
+    
+    public int calculateMove(){
 		Random random = new Random();
-		move = random.nextInt(9);
-		while(movesDone.contains(move)){
-			move = random.nextInt(9);
-		}
-		//processMove(move, 'ç');
-		return move;
+        move = random.nextInt(64);
+        while(movesDone.contains(move)){
+            move = random.nextInt(9);
+        }
+        return move;
 	}
 
+    @Override
+    public void processMove(int input, char c) {
+        movesDone.add(input);
+        super.board = adaptBoard(input,c);
+    }
+
+    private String adaptBoard(int input, char c){
+        if(input<0 || input>8){
+            return super.board;
+        }
+        char[] chars = super.board.toCharArray();
+        chars[input] = c;
+        return String.valueOf(chars);
+    }
+    public void printBoard(){
+        System.out.println(super.board);
+    }
+
 	@Override
-	public void processMove(int input, char c) {
-		movesDone.add(input);
-		super.board = adaptBoard(input,c);
+	void reset() {
+		super.board = "#########";
+		movesDone.clear();
 	}
-	
-	private String adaptBoard(int input, char c){
-		if(input<0 || input>=8){
-	        return super.board;
-	    }
-	    char[] chars = super.board.toCharArray();
-	    chars[input] = c;
-	    return String.valueOf(chars);  
-	}
-	public void printBoard(){
-		System.out.println(super.board);
-	}
-	
-	/* test code
-	public static void main(String [ ] args) {
-		TicTacToeAI ai = new TicTacToeAI();
-		ai.printBoard();
-		System.out.println(ai.getNewMove(1));
-		ai.printBoard();
-		System.out.println(ai.getNewMove(5));
-		ai.printBoard();
-	}
-	*/
 }
